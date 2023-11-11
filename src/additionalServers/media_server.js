1e00443644971f280e7c89efb954167aecdb4b73
jeka0 <latosh02@mail.ru>
2023-11-11 23:29:52 +0300

    added thumbnails

--- src/additionalServers/media_server.js
+++ src/additionalServers/media_server.js
@@ -1,5 +1,5 @@
 const { getUserByStreamKey } = require('../services/userService');
-const { generateStreamThumbnail } =require('../helpers/thumbnail');
+const { generateStreamThumbnail, delay } =require('../helpers/thumbnail');
 const streamSevice = require('../services/streamService');
 const NodeMediaServer = require('node-media-server'),
     config = require('../../config/default').rtmp_server;
@@ -14,13 +14,18 @@ nms.on('prePublish', async (id, StreamPath, args) => {
         session.reject();
     } else {
         streamSevice.createStream( user.id, { stream_title:"AAAAAAAAAA", category: "games" });
-        generateStreamThumbnail(stream_key);
         console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
     }
 });
 
+nms.on('postPlay', async (id, StreamPath, args) =>{
+    delay(10000).then(() => {
+        let stream_key = getStreamKeyFromStreamPath(StreamPath);
+        generateStreamThumbnail(stream_key);
+    });
+});
+
 nms.on('donePublish', async (id, StreamPath, args) => {
-    console.log('donePublish');
     let stream_key = getStreamKeyFromStreamPath(StreamPath);
     const user = await getUserByStreamKey(stream_key);
     streamSevice.finishStream(user.id);
--- src/helpers/thumbnail.js
+++ src/helpers/thumbnail.js
@@ -1,15 +1,39 @@
 const spawn = require('child_process').spawn,
     config = require('../../config/default'),
     cmd = config.rtmp_server.trans.ffmpeg;
-const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs');
+
+const delay = async (ms) => await new Promise(resolve => setTimeout(resolve, ms));
 
 const generateStreamThumbnail = (stream_key) => {
+    const args = [
+        '-y',
+        '-i', `http://127.0.0.1:8888/live/${stream_key}/index.m3u8`,
+        '-ss', '1',
+        '-t', '1',
+        '-f', 'mjpeg',
+        `./server/thumbnails/${stream_key}.png`,
+    ];
 
+    const child = spawn(cmd, args);
 
-
+    child.on('start', () => {
+        console.log('Дочерний процесс запущен');
+      });
+      
+      child.on('exit', (code) => {
+        if (code === 0) {
+          console.log('Дочерний процесс завершился успешно');
+        } else {
+          console.log('Дочерний процесс завершился с ошибкой');
+        }
+      });
+      
+      child.on('error', (err) => {
+        console.error('Ошибка дочернего процесса:', err);
+      });
 };
 
 module.exports = {
-    generateStreamThumbnail
+    generateStreamThumbnail,
+    delay
 };
